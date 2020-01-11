045278506b43bb5492626da9c2ebbc381a20f24e
manhluna <tuhoc98@gmail.com>
2020-01-11 13:25:48 +0700

    1.0.6

--- lang/en.js
+++ lang/en.js
@@ -44,7 +44,7 @@ const tranMail = {
     SUBJECT: "WBANK EXCHANGE: Authenticate Account",
     TEMPLATE: (linkVerify) => {
         return `
-        <div style=""width: 100%">
+        <div style=""width: 100%; text-align: center;">
             <img src="https://i.imgur.com/6PfRAgh.png" style="width:25%";>
             <h2>Thank you for your join in WBANK Global</h2>
             <h3>Please Click the link to complete the registation</h3>
