210c91f0dea3fb350beefeb855743da941dd8c27
manhluna <tuhoc98@gmail.com>
2020-01-11 14:39:57 +0700

    v1.2.0

--- lang/en.js
+++ lang/en.js
@@ -45,7 +45,7 @@ const tranMail = {
     TEMPLATE: (linkVerify) => {
         return `
         <div style=""width: 100%; text-align: center;">
-            <img src="https://i.imgur.com/6PfRAgh.png" style="width:25%";>
+            <img src="https://i.imgur.com/aHrdCGz.jpg" style="width:25%";>
             <h2>Thank you for your join in WBANK Global</h2>
             <h3>Please Click the link to complete the registation</h3>
             <br>
