9a680e00eddcdd49fabcd9b10714fb84b23eedb1
manhluna <tuhoc98@gmail.com>
2020-01-17 00:58:18 +0700

    a

--- src/app.js
+++ src/app.js
@@ -66,6 +66,27 @@ io.use(passportSocketIo.authorize({
 //config socket;
 initSockets(io);
 
+let servers = https.createServer(sslOptions, app)
+let ios = socketio(servers)
+ios.use(passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: process.env.SESSION_KEY,
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    success: (data, accept) => {
+        if(!data.user.logged_in){
+            return accept("Invalid User", false);
+        }
+        return accept(null, true);
+    },
+    fail: (data, message, error, accept) => {
+        if(error){
+            console.log("failed connection to socket.io: ", message );
+            return accept(new Error(message), false);
+        }
+    }
+}))
+initSockets(ios)
 
 server.listen(process.env.APP_PORT, ()=>{
     console.log(` listening on port: ${process.env.APP_PORT}`);
