e32ceaff135a77d918b34e5304c27e92986474eb
manhluna <tuhoc98@gmail.com>
2020-01-19 01:47:54 +0700

    19012020v1

--- src/services/price.js
+++ src/services/price.js
@@ -1,9 +1,10 @@
 const DB = require('./../models/walletModel')
+const delay = require('delay')
 const binance = require('node-binance-api')().options({
   APIKEY: '6eKAHwLiaEWJY4F0KmNazf0qEp0llEExCcQUY5YzE1HrCvm53iUGWHtZ0EMu1GCa',
   APISECRET: 'hBEo6hJwYkwAGbxXBxdCGoe9QgKdDthcwsm16eeBkEHowOjnDIdRE6tHpJkmeFj9',
   useServerTime: true // If you get timestamp errors, synchronize to server time at startup
-});
+})
 
 const price = (coin,cb)=>{
   if (coin === 'btc') {
@@ -19,10 +20,13 @@ const price = (coin,cb)=>{
   }
 }
 const wsPrice = (cb)=>{
-  binance.websockets.trades(['BTCUSDT'], (trades) => {
-    let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = trades
-    cb(price)
-  });
+  // binance.websockets.trades(['BTCUSDT'], (trades) => {
+  //   let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = trades
+  //   cb(price)
+  // });
+  setInterval(()=>{
+    price('btc', x => cb(x))
+  },8000)
 }
 
 
--- src/sockets/notiSocket.js
+++ src/sockets/notiSocket.js
@@ -180,7 +180,7 @@ let postDeposit = (io) => {
                 }
             });
         }
-        later.setInterval(a, later.parse.text('every 1 seconds'))
+        later.setInterval(a, later.parse.text('every 8 seconds'))
         socket.on("disconnect", ()=> {
             clients[currentUserId] = clients[currentUserId].filter((socketId)=>{
                 return socketId !== socket.id;
